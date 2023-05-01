369140b9467baf156fe1fbe83cf50bab3265be77
dellylucas <dellylucas@hotmail.com>
2023-05-01 00:19:55 -0500

    first

--- app/src/main/java/com/dfl/viewalwaysblockmessage/MainActivity.kt
+++ app/src/main/java/com/dfl/viewalwaysblockmessage/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.dfl.viewalwaysblockmessage
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val lockScreenReceiver = LockScreenReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
-
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_SCREEN_OFF)
+            addAction(Intent.ACTION_SCREEN_ON)
+        }
+        registerReceiver(lockScreenReceiver, filter)
+    }
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(lockScreenReceiver)
+    }
 }
